dd7ae15b147e3cce51843727fa3834fe8bd4e17a
Hugo Agbonon <hugo@agbonon.fr>
2014-05-15 01:03:44 +0200

    Sending back JSON

--- controllers/auth.js
+++ controllers/auth.js
@@ -63,15 +63,15 @@ module.exports = function(app) {
     req.logout();
     res.redirect('/');
   });
+
+  // route middleware to make sure
+  function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+      return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+  }
 };
-
-// route middleware to make sure
-function isLoggedIn(req, res, next) {
-
-  // if user is authenticated in the session, carry on
-  if (req.isAuthenticated())
-    return next();
-
-  // if they aren't redirect them to the home page
-  res.redirect('/');
-}
--- controllers/pokes.js
+++ controllers/pokes.js
@@ -4,8 +4,25 @@ var Poke = require('../models/poke');
 var User = require('../models/user');
 
 module.exports = function (app) {
+
+  function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+
+    res.jsonp(
+      401,
+      {
+        title: 'Unauthorized',
+        detail: 'Login needed'
+      }
+    );
+  }
+
   // Before getting here, user must be logged in
   // if logged in, his data must be in req.loggedInUser
+
+  app.get('/pokes', isLoggedIn, function(req, res) {
+    res.jsonp(req.user.toPublicJSON());
+  });
 
   app.get('/pokes/:opponentEmail', function (req, res) {
     // getting to sleep, now starting to wonder why everything's not on a single big collection
