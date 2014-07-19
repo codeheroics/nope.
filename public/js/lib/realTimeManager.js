c42a4bdbdc2de7b6903855491005a5e349c76678
Hugo Agbonon <hugo@agbonon.fr>
2014-07-19 02:20:19 +0200

    Connect to websocket & authenticate correctly

--- public/js/lib/realTimeManager.js
+++ public/js/lib/realTimeManager.js
@@ -11,17 +11,22 @@ PokeGame.RealTimeManager = Ember.Object.extend({
 
   init: function() {
     var self = this;
-    this.primus = Primus.connect(PRIMUS_ROUTE);
+    this.primus = Primus.connect(
+      PRIMUS_ROUTE, { strategy : ['online', 'disconnect'] }
+    );
 
     //
     // For convenience we use the private event `outgoing::url` to append the
     // authorization token in the query string of our connection URL.
     //
     this.primus.on('outgoing::url', function connectionURL(url) {
-      console.log('ii');
-      url.query = 'token=' + (localStorage.getItem('token') || '');
+      url.query = 'access_token=' + (localStorage.getItem('token') || '');
     });
 
     this.primus.on('data', this.getPoked);
+
+    this.primus.on('error', function error(err) {
+      console.error('Something horrible has happened', err);
+    });
   }
 });
