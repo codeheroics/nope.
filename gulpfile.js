29cf029857c0ab3a9c61000e07518e1e54e7e265
Hugo Agbonon <hugo@agbonon.fr>
2014-09-18 01:21:54 +0200

    gulp buildProd

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ var minifyHTML = require('gulp-minify-html');
 var minifyCSS = require('gulp-minify-css');
 var useref = require('gulp-useref');
 var merge = require('merge-stream');
+var gulpif = require('gulp-if');
 
 var DEV_PUBLIC = './public';
 var DIST_PUBLIC = './public_dist';
@@ -22,6 +23,10 @@ function appendDevPublic (src) {
 
 gulp.task('default', ['watch']);
 
+gulp.task('clean', function (cb) {
+  rimraf(DIST_PUBLIC, cb);
+});
+
 gulp.task('watch', function() {
   gulp.watch('public/**/*.css', ['buildCSS']);
   gulp.watch('public/**/*.js', ['buildJS']);
@@ -29,6 +34,7 @@ gulp.task('watch', function() {
 });
 
 gulp.task('build', ['buildHTML', 'buildJS', 'buildCSS']);
+gulp.task('buildProd', ['buildHTML', 'buildProdJS', 'buildCSS']);
 
 gulp.task('buildHTML', function() {
   gulp.src(DEV_PUBLIC + '/app.html')
@@ -39,55 +45,11 @@ gulp.task('buildHTML', function() {
     .on('error', gutil.log);
 });
 
-gulp.task('buildJS', function() {
-  var jsComponents = [
-    'components/jquery/dist/jquery.js',
-    'components/moment/moment.js',
-    'components/handlebars/handlebars.js',
-    'components/ember/ember.js',
-    'components/ember-simple-auth/ember-simple-auth.js',
-    'components/ember-model/ember-model.js',
-    'components/ember-model-localstorage-adapter/ember-model-localstorage-adapter.js',
-    'components/toastr/toastr.js',
-    'components/js-md5/js/md5.js'
-  ];
-
-  var jsDependencies = [
-    'primus/primus.js',
-    'js/animatedBorders/borderMenu.js',
-    'js/animatedBorders/classie.js',
-    'js/animatedBorders/modernizr.custom.js',
-    'js/skel.min.js'
-  ];
-
-  var jsApp = [
-    'js/app.js',
-    'js/router.js',
-    'js/lib/*.js',
-    'js/models/*.js',
-    'js/controllers/*.js'
-  ];
-
-  var sources = jsComponents
-  .concat(jsDependencies)
-  .concat(jsApp)
-  .map(appendDevPublic);
-
-  return gulp.src(sources)
-    .pipe(concat('build.js'))
-    .pipe(gulp.dest(DIST_PUBLIC))
-    .pipe(size({showFiles: true}))
-    .pipe(uglify())
-    .pipe(rename('build.min.js'))
-    .pipe(size({showFiles: true}))
-    .pipe(gulp.dest(DIST_PUBLIC))
-    .on('error', gutil.log);
-});
 
 gulp.task('buildCSS', function() {
   var cssComponents = [
-  'components/toastr/toastr.css',
-  'components/font-awesome/css/font-awesome.css'
+  'components/toastr/toastr.min.css',
+  'components/font-awesome/css/font-awesome.min.css'
   ];
 
   var cssDependencies = [
@@ -104,10 +66,9 @@ gulp.task('buildCSS', function() {
 
   var css = gulp.src(sources)
     .pipe(concat('build.css'))
-    .pipe(size({showFiles: true}))
-    .pipe(gulp.dest(cssDir))
+    // .pipe(gulp.dest(cssDir))
     .pipe(minifyCSS())
-    .pipe(rename('build.min.css'))
+    // .pipe(rename('build.css'))
     .pipe(size({showFiles: true}))
     .pipe(gulp.dest(cssDir))
     .on('error', gutil.log);
@@ -129,6 +90,63 @@ gulp.task('buildCSS', function() {
   return merge(css, ieCss, fonts, images);
 });
 
-gulp.task('clean', function (cb) {
-  rimraf(DIST_PUBLIC, cb);
-});
+function buildJSFromSources(isProdBuild) {
+  var jsComponents = [
+    'components/jquery/dist/jquery.js',
+    'components/moment/moment.js',
+    'components/handlebars/handlebars.js',
+    'components/ember/ember.js',
+    'components/ember-simple-auth/ember-simple-auth.js',
+    'components/ember-model/ember-model.js',
+    'components/ember-model-localstorage-adapter/ember-model-localstorage-adapter.js',
+    'components/toastr/toastr.js',
+    'components/js-md5/js/md5.js'
+  ];
+
+  var jsMinComponents = [
+    'components/jquery/dist/jquery.min.js',
+    'components/moment/min/moment.min.js',
+    'components/handlebars/handlebars.min.js',
+    'components/ember/ember.prod.js',
+    'components/ember-simple-auth/ember-simple-auth.js',
+    'components/ember-model/ember-model.js',
+    'components/ember-model-localstorage-adapter/ember-model-localstorage-adapter.js',
+    'components/toastr/toastr.min.js',
+    'components/js-md5/js/md5.min.js'
+  ];
+
+  var jsDependencies = [
+    'primus/primus.js',
+    'js/animatedBorders/borderMenu.js',
+    'js/animatedBorders/classie.js',
+    'js/animatedBorders/modernizr.custom.js'
+  ];
+
+  var jsApp = [
+    'js/app.js',
+    'js/router.js',
+    'js/lib/*.js',
+    'js/models/*.js',
+    'js/controllers/*.js'
+  ];
+
+  var jsSources = jsComponents
+  .concat(jsDependencies)
+  .concat(jsApp)
+  .map(appendDevPublic);
+
+  var jsMinSources = jsMinComponents
+  .concat(jsDependencies)
+  .concat(jsApp)
+  .map(appendDevPublic);
+
+  return gulp.src(isProdBuild ? jsMinSources : jsSources)
+    .pipe(concat('build.js'))
+    .pipe(gulpif(isProdBuild, uglify()))
+    .pipe(size({showFiles: true}))
+    .pipe(gulp.dest(DIST_PUBLIC))
+    .on('error', gutil.log);
+}
+
+gulp.task('buildJS', buildJSFromSources.bind(null, false));
+gulp.task('buildProdJS', buildJSFromSources.bind(null, true));
